983a7c814c22b29a8c4b3bb53a2c4618df9214e7
Nishant Namboothiry <22nambon@elmbrookstudents.org>
2018-09-13 13:18:06 -0500

    I can kind of code now
    I can code.

---
+++ src/outputExamples/FirstLastAge.java
@@ -0,0 +1,13 @@
+package outputExamples;
+
+public class FirstLastAge {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Nishant");
+			System.out.println("Namboothiry");
+			System.out.println("Age:14");
+
+	}
+	
+}
---
+++ src/outputExamples/name.java
@@ -0,0 +1,5 @@
+package outputExamples;
+
+public class name {
+
+}
