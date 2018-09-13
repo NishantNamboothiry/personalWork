428939fc07bedd214dc767ac8d63c57416eb1528
Nishant Namboothiry <22nambon@elmbrookstudents.org>
2018-09-13 13:32:50 -0500

    Deleted Something

---
+++ src/outputExamples/FirstLastAgeVote.java
@@ -0,0 +1,14 @@
+package outputExamples;
+
+public class FirstLastAgeVote {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Nishant");
+			System.out.println("Namboothiry");
+			System.out.println("Age:14");
+			System.out.println("Years until voting allowed:4");
+
+	}
+
+}
