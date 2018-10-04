0da736de734154adf2cde7d920d83382d124036f
Nishant Namboothiry <22nambon@elmbrookstudents.org>
2018-10-04 14:09:18 -0500

    More OOP

--- src/Greeter/CarOOP.java
+++ src/Greeter/CarOOP.java
@@ -1,18 +1,40 @@
 package Greeter;
 
+import java.util.Scanner;
+
 public class CarOOP {
 	public CarOOP() {
 		
 	}
-	public void Odometer() {
-		System.out.println("378954");
+	public void Area() {
+		int length = 0;
+		int height = 0;
+	Scanner input = new Scanner(System.in);	
+System.out.println("What is the length of the wall?");
+	length = input.nextInt();
+	System.out.println("What is the height of the wall?");
+	height = input.nextInt();
+	
+	System.out.println("The area of your wall is "+(length*height));
+
+	}
+
 		
 			
-	}
-public void Flat(){
-	System.out.println("You have 4 flat tires");
 	
-}	
+public void door() {
+	int length = 0;
+	int height = 0;
+Scanner input = new Scanner(System.in);	
+System.out.println("What is the length of the door?");
+length = input.nextInt();
+System.out.println("What is the height of the door?");
+height = input.nextInt();
+
+System.out.println("The area of your door is "+(length*height));
+
+}
+	
 public void Thirsty() {
 	System.out.println("Hey dude, how are you? I need some gas, or I'm gonna die. Thx!");
 	}
---
+++ src/Greeter/Roomclass.java
@@ -0,0 +1,14 @@
+package Greeter;
+
+import java.util.Scanner;
+
+public class Roomclass {
+	
+	public static void main(String[] args) {
+		CarOOP wall= new CarOOP();
+	wall.Area();
+	wall.door();
+	
+	}
+}
+		
