713c7e9796c09a0c7e0d617927c0a88551bc3079
Unknown <22nambon@elmbrookstudents.org>
2018-10-26 10:57:14 -0500

    More Loops

--- src/decisions/NumberTest.java
+++ src/decisions/NumberTest.java
@@ -1,17 +1,19 @@
 package decisions;
 import java.util.Scanner;
 
-public class HelloTest {
+public class NumberTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input= new Scanner(System.in);
 		int counter=0;
-		double number=0;
 		int sum= 0;
+		int number= 0;
 		{
-		for (counter=1;counter<=10;counter= counter+1 )
+		for (counter=1;counter<=5;counter++ )
 		{
-			sum+=counter;
+			System.out.println("What is your number?");
+			number=input.nextInt();
+			sum+=number;
 		}
 		System.out.println(sum);
 
